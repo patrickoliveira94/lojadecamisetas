4837adfc396797fc032af0b0979c0d430c4e6627
patrickoliveira94 <oliveira.patrick94@gmail.com>
2018-03-13 17:51:00 -0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :produtos, only: [:new, :create, :destroy]
+  root "produtos#index"
+
+  get "/produtos/busca" => "produtos#busca", as: :busca_produto
+end
